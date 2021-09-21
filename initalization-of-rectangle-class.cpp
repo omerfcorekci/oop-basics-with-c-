6e0fd1b9729edb271e434e597c48448f3b0eb214
Ömer Faruk Çörekci <omerfarukcorekci@gmail.com>
2021-09-22 02:23:32 +0300

    Create initalization-of-rectangle-class.cpp

--- initalization-of-rectangle-class.cpp
+++ initalization-of-rectangle-class.cpp
@@ -15,6 +15,6 @@ int main(){
     rectangle exp1,exp2;
     exp1.widht=50; exp1.height=100;
     exp2.widht=150; exp2.height=200;
-    cout << exp1.findOfArea() << " " << exp1.findOfPerimeter() << "\n" << exp2.findOfArea() << " " << exp2.findOfPerimeter() << endl;
+    cout << "Area and Perimeter of Rectangle1 : " << exp1.findOfArea() << " " << exp1.findOfPerimeter() << "\nArea and Perimeter of Rectangle2 : " << exp2.findOfArea() << " " << exp2.findOfPerimeter() << endl;
     return 0;
 }
